4dfe03f11a803cabed36d4ff465f07010dcb45d0
itgbarbara <itgbarbara@gmail.com>
2024-07-11 15:14:45 +0200

    Layout reorganization

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,15 @@
 import Header from '../components/Header'
 import '../utils/styles/global.scss'
+import Footer from './Footer'
 
 function Layout({ children }) {
   return (
-    <div className="main-container">
-      <Header />
-      <main>{children}</main>
+    <div>
+      <div className="layout">
+        <Header />
+        <main className="layout__page">{children}</main>
+      </div>
+      <Footer />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,12 +5,10 @@ import ReactDOM from 'react-dom/client'
 import './utils/styles/global.scss'
 // Import des composants
 import Router from './components/Router'
-import Footer from './components/Footer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Router />
-    <Footer />
   </React.StrictMode>
 )
