aa72e7250bef0484b6f88865510c4f126e12bd38
itgbarbara <itgbarbara@gmail.com>
2024-07-11 14:32:32 +0200

    Adding a Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import Logo from '../assets/images/logo-footer.svg'
+import '../utils/styles/global.scss'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <img src={Logo} alt="logo Kasa" className="footer__logo" />
+      <span>© 2020 Kasa. All rights reserved</span>
+    </footer>
+  )
+}
+
+export default Footer
