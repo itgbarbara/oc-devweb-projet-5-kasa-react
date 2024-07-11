64216cec368c543680354e148bab21908160555b
itgbarbara <itgbarbara@gmail.com>
2024-07-11 21:50:59 +0200

    Adding content and style to the Error page#	src/utils/styles/logement.scss

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,10 +1,19 @@
+import { Link } from 'react-router-dom'
 import Layout from '../components/Layout'
-import '../utils/styles/global.scss'
+import Error from '../assets/images/404.svg'
 
 function ErrorPage() {
   return (
     <Layout>
-      <h2>Erreur 404</h2>
+      <div className="errorpage">
+        <img src={Error} alt="Error 404" className="errorpage__img" />
+        <p className="errorpage__content">
+          Oups! La page que vous demandez n'existe pas
+        </p>
+        <Link to="/" className="errorpage__link">
+          Retourner sur la page d'accueil
+        </Link>
+      </div>
     </Layout>
   )
 }
