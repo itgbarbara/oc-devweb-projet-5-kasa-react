f49b1ccd3dac372f4e83f3e3e2cdbde7a0bbc1e8
itgbarbara <itgbarbara@gmail.com>
2024-07-11 21:58:00 +0200

    Creating and adding style to the 'Logement' page, and adding a route to the router

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About'
 import ErrorPage from '../pages/ErrorPage'
+import Logement from '../pages/Logement'
 
 function Router() {
   return (
@@ -9,6 +10,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/logement/:id" element={<Logement />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Logement.js
@@ -0,0 +1,49 @@
+import Carousel from '../components/Carousel'
+import Layout from '../components/Layout'
+import locations from '../data/logements.json'
+import { useParams } from 'react-router-dom'
+
+function Logement() {
+  // On récupère l'id de l'url
+  const { id } = useParams()
+
+  // Méthode pour trouver l'index de l'élément dont l'id est dans l'url de la fiche logmement
+  const index = locations.findIndex((location) => location.id === id)
+
+  return (
+    <Layout>
+      <div className="fiche-container">
+        <div className="infos-container">
+          <div className="infos">
+            <h2 className="infos__title">{locations[index].title}</h2>
+            <span className="infos__location">{locations[index].location}</span>
+            <div className="infos__tags">
+              <ul>
+                <li>{locations[index].tags[0]}</li>
+                <li>{locations[index].tags[1]}</li>
+                <li>{locations[index].tags[2]}</li>
+              </ul>
+            </div>
+          </div>
+          <div className="infos-2">
+            <div className="host">
+              <span className="host__name">{locations[index].host.name}</span>
+              <img
+                className="host__picture"
+                src={locations[index].host.picture}
+                alt=""
+              />
+            </div>
+            <div className="ranking">Rating : {locations[index].rating}</div>
+          </div>
+        </div>
+        <div className="description">
+          <h3 className="description__item">Description</h3>
+          <h3 className="description__item">Equipement</h3>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default Logement
