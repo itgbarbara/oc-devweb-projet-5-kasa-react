fb208a75d74e7f33fb39fe2b7d1d289a0423db0f
itgbarbara <itgbarbara@gmail.com>
2024-07-15 21:02:09 +0200

    adding a linear gradient to the cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,13 +5,14 @@ function Cards() {
   return (
     <div className="cards-container">
       {locations.map((location) => (
-        <Link to={`/logement/${location.id}`}>
-          <article className="card" key={location.id}>
+        <Link to={`/logement/${location.id}`} key={location.id}>
+          <article className="card">
             <img
               className="card__img"
               src={location.cover}
               alt={location.title}
             />
+            <div className="card__cover"></div>
             <h2 className="card__title">{location.title}</h2>
           </article>
         </Link>
